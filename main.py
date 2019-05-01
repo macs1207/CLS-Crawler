cb81fa9e8ffe210fdbc5911e0e4e9ba8f81929ec
macs1207 <kenginlim@gmail.com>
2019-05-02 02:45:47 +0800

    Modify the project architecture.

--- main.py
+++ main.py
@@ -6,45 +6,8 @@ import json
 import logging
 import re
 from lxml import etree
-from mailSender import sendMail
-
-
-class fileLoad():
-    def __init__(self, fileName):
-        self.failName = fileName
-        try:
-            with open(self.failName, "r") as f:
-                self.d = json.loads(f.read())
-        except IOError:
-            data = {
-                "loginFailed": "7364cbf35aa828ba6aa5a9334faf4645",
-                # This is the page hash when cookies are lost or gone.
-                "failed": "a9897117eec5eee62199f4d063824446",
-                "content": {}
-            }
-            self.d = data
-            with open(self.failName, "w") as f:
-                f.write(json.dumps(data))
-
-    def clear(self):
-        with open(self.failName, "w") as f:
-            f.seek(0)
-            f.truncate()
-
-    def update(self, lstHw):
-        self.clear()
-        self.d["content"] = lstHw
-        with open(self.failName, "w") as f:
-            f.write(json.dumps(self.d))
-
-    def failedHash(self):
-        return self.d["failed"]
-
-    def getCache(self):
-        return self.d["content"]
-
-    def loginFailed(self):
-        return self.d["loginFailed"]
+from Module.MailSender import sendMail
+from Module import FileProcess
 
 
 def callSend(content):
@@ -120,7 +83,7 @@ def getCookies():
         r = requests.post(url, data=payLoad["user"])
         r.encoding = "utf-8"
         curHash = hashlib.md5(r.text.encode("utf-8")).hexdigest()
-        if curHash == fileLoad("data.json").loginFailed():
+        if curHash == FileProcess.fileLoad("data.json").loginFailed():
             print("Login Failed! Input your info again.")
             confIpt(False, payLoad)
         else:
@@ -133,7 +96,7 @@ def main():
     url = "http://cls.kuas.edu.tw/admin/managehomework/uploadhw"
     payLoad = {"course_id": 73625}
     cookies = getCookies()
-    data = fileLoad("data.json")
+    data = FileProcess.fileLoad("data.json")
     failedHash = data.failedHash()
     while True:
         # Fetch the informamtion of cls every five minutes.
